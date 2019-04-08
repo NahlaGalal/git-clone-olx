38c3ba01aae6c589ba1fca03e1fcceb8a9ac133f
Nahla Galal <nahlaglal@gmail.com>
2019-04-09 00:57:59 +0200

    Add item component state

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,31 +5,50 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../style/item.css';
 
 export default class Item extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: 'Laptop Dell - inspiron n4050',
+            price: 7800,
+            images: [
+                '../Images/lab1.jpg',
+                '../Images/lab2.jpg',
+                '../Images/lab3.jpeg'
+            ],
+            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?` ,
+            buyer: {
+                name: 'Nahla5',
+                phone: '01001112345',
+                email: 'nahlaglal@gmail.com',
+                city: 'Mansoura'
+            }
+        }
+    }
 
     render() {
         return (
             <div className="container item">
                 <main>
                     <div className="info">
-                        <h1> Laptop Dell - inspiron n4050 </h1>
+                        <h1> {this.state.name} </h1>
                         <hr />
-                        <p className="price"> <strong> 7,800 LE. </strong> </p>
+                        <p className="price"> <strong> {this.state.price} LE. </strong> </p>
                     </div>
                     <Carousel showStatus={false} autoPlay={true} infiniteLoop={true}>
                         <div><img src={require('../Images/lab1.jpg')} alt="First for product"/></div>
-                        <div><img src={require('../Images/lab2.jpg')} alt="Second for product"/></div>
-                        <div><img src={require('../Images/lab3.jpeg')} alt="Third for product"/></div>
+                        <div><img src={require('../Images/lab2.jpg')} alt="First for product"/></div>
+                        <div><img src={require('../Images/lab3.jpeg')} alt="First for product"/></div>
                     </Carousel>
                     <h2> Description </h2>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci? </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, beatae repellendus. Pariatur asperiores, beatae dolores laboriosam deleniti nobis eius esse minus dolore suscipit facilis commodi nesciunt nemo a praesentium adipisci?</p>
+                    <p> {this.state.description} </p>
                 </main>
                 <aside>
-                    <p> <strong> 7,800 LE. </strong> </p>
+                    <p> <strong> {this.state.price} LE. </strong> </p>
                     <button>Buy now</button> 
                     <hr />
                     <h2>Product Information</h2>
@@ -44,13 +63,13 @@ export default class Item extends Component {
                     <h2> Buyer Information </h2>
                     <dl>
                         <dt>User Name</dt>
-                        <dd>Nahla5</dd>
+                        <dd>{this.state.buyer.name}</dd>
                         <dt>Phone</dt>
-                        <dd>01001112345</dd>
+                        <dd>{this.state.buyer.phone}</dd>
                         <dt>E-mail</dt>
-                        <dd><a href="mailto:nahlaglal@gmail.com">nahlaglal@gmail.com</a></dd>
+                        <dd><a href={`mailto:${this.state.buyer.email}`}>{this.state.buyer.email}</a></dd>
                         <dt>City</dt>
-                        <dd>Mansoura</dd>
+                        <dd>{this.state.buyer.city}</dd>
                     </dl>
                 </aside>
                 <button>Buy Now</button>
