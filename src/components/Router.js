a95b07f8d611bfc6d4e895e5ca484bec7c2b0dbc
Nahla Galal <nahlaglal@gmail.com>
2019-04-13 20:51:07 +0200

    Modify routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Layout from './Layout';
 import Item from './Item';
 import Home from './Home';
 import Signup from './Signup';
+import Error from './Error';
 
 export default class Router extends Component {
     render() {
@@ -11,9 +12,13 @@ export default class Router extends Component {
             <BrowserRouter>
                 <Switch>
                     <Layout>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/item" component={Item} />
-                        <Route exact path="/signup" component={Signup} />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/item" component={Item} />
+                            <Route exact path="/signup" component={Signup} />
+                            <Route exact path="/error" component={Error} />
+                            <Redirect to="/error" />
+                        </Switch>
                     </Layout>
                 </Switch>
             </BrowserRouter>
